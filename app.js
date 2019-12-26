70a3c6a390eb81ae2e118c6cb07e75db2062a3dd
git-neelesh <uno.neelesh@gmail.com>
2019-12-27 00:04:38 +0530

    environment wise db conn

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const graphqlHTTP = require('express-graphql')
+if (process.NODE_ENV === 'development')
+    var DB_PATH = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-hl4dh.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`;
+else
+    var DB_PATH = 'mongodb://localhost:27017/graphql_sample?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false';
 
 const mongoose = require('mongoose');
 
@@ -17,7 +21,7 @@ app.use('/graphql', graphqlHTTP({
 }));
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/hello', (req, res, next) => res.send('Hello World!'))
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-hl4dh.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(DB_PATH, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("Started")
         app.listen(3000);
